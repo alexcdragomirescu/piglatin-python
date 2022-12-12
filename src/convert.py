e5406031979c1e219765b1b2d975bdfd6063cd28
Alexandru Constantin Dragomirescu <alexandru@dragomirescu.net>
2022-12-12 21:20:38 +0200

    Add pigify

---
+++ src/convert.py
@@ -0,0 +1,25 @@
+import argparse
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+def pigify(text):
+    words = text.split(' ')
+    piglatinWords = []
+
+    for word in words:
+        if (word[0] in vowels):
+            pigWord = word + 'ay'
+        else:
+            pigWord = word[1:] + word[0] + 'ay'
+        piglatinWords.extend([pigWord])
+    return ' '.join(piglatinWords)
+
+
+parser = argparse.ArgumentParser(
+    'convert.py',
+    description=''
+)
+parser.add_argument('input', type=str)
+args = parser.parse_args()
+
+print(pigify(args.input))
